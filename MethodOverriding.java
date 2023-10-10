37cfdde6dae3c6379c6edf9dd6d985240dc8ae4a
pksingh1509 <rajiit1212@gamil.com>
2023-10-10 11:14:18 +0530

    fix some mix code

--- MethodOverriding.java
+++ MethodOverriding.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class Base12 {
     int x;
     
@@ -13,27 +13,13 @@ class Derived12 extends Base12 {
     void eating() {
         x = 34;
         System.out.println("Derived is eating...");
-=======
-class Upper {
-    int x = 12;
 
-    void show() {
-        x = 34;
-        System.out.println("This is a method of upper class");
-    }
-}
-
-class Niche extends Upper {
-    void show() {
-        x = 90;
-        System.out.println("This is a method of Niche class");
->>>>>>> cf18492bde9d5f7f6e78b69820499abffc7781bb
     }
 }
 
 public class MethodOverriding {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
 
         
         Base12 base = new Derived12();
@@ -41,14 +27,6 @@ public class MethodOverriding {
 
         Base12 b = new Base12();
         b.eating();
-
-    
-=======
-        Upper n = new Niche();
-        n.show();
-        Upper u = new Upper();
-        System.out.println(u.x + " : " + n.x);
-
->>>>>>> cf18492bde9d5f7f6e78b69820499abffc7781bb
+       
     }
 }
